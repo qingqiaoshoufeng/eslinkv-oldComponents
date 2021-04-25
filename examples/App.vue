03ad8c97891603d5d1a326faa245fb11f8bdfa3e
zhepei.shen <zhepei.shen@eslink.com>
2021-04-25 11:26:43 +0800

    feat: update npm version

--- examples/App.vue
+++ examples/App.vue
@@ -5,46 +5,13 @@
 </template>
 <script lang="ts">
 import { Component, Vue, Provide } from 'vue-property-decorator'
-import {
-	dScreen,
-	platform,
-	commonConfigValue,
-	configMerge,
-	dDetail,
-} from 'eslinkv-sdk'
-import custom from '../packages/o-pie-8/custom.vue'
-import {
-	value,
-	customConfig,
-	version,
-	type,
-} from '../packages/o-pie-8/index.component'
+import { dScreen, platform, dDetail } from 'eslinkv-sdk'
 
 @Component({ components: { dScreen, dDetail } })
 export default class App extends Vue {
 	platform = platform.state
 
 	@Provide('kanboardEditor') kanboardEditor = this
-
-	created() {
-		Vue.component(`market-${type}-${version}`, custom)
-		setTimeout(() => {
-			const id = +new Date()
-			const config = configMerge(value, commonConfigValue())
-			config.layout.position.left = 500
-			config.layout.position.top = 500
-			config.customConfig = customConfig
-			platform.state.widgetAdded = {
-				[id]: {
-					id,
-					type,
-					config,
-					scene: 0,
-					market: true,
-				},
-			}
-		}, 2000)
-	}
 }
 </script>
 <style lang="scss">
