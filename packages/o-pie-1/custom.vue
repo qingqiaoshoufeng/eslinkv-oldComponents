5eb1fd79ec30b374dfaca91adbb7a98b3136f80f
xjm <jiaming.xu@eslink.com>
2021-05-07 10:24:15 +0800

    fix: 饼图点击事件

--- packages/o-pie-1/custom.vue
+++ packages/o-pie-1/custom.vue
@@ -19,6 +19,7 @@ export default class OLine1 extends mixins(widgetMixin) {
 			this.$nextTick(() => {
 				this.instance = echarts.init(document.getElementById(this.id))
 				this.setOption(data, this.config.config)
+				this.instance.off('click')
 				this.instance.on('click', params => {
 					this.__handleClick__(params.data)
 				})
