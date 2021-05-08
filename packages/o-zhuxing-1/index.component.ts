fbe83266e41991548642e1104be00ed768075253
zhepei.shen <zhepei.shen@eslink.com>
2021-05-08 17:24:24 +0800

    feat: add build js

--- packages/o-zhuxing-1/index.component.ts
+++ packages/o-zhuxing-1/index.component.ts
@@ -3,7 +3,7 @@ export const name = '单轴图'
 // 组件名唯一
 export const type = 'o-zhuxing-1'
 // 检测当前组件名，版本唯一，类似npm发布策略
-export const version = '1.0.5'
+export const version = '1.0.6'
 // 自定义配置
 export const customConfig = [
 	{
