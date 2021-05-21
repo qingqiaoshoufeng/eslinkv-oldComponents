fb24067ada4d26755b7f994e6d491cff79731e40
xjm <jiaming.xu@eslink.com>
2021-05-21 09:41:09 +0800

    feat: add select6

---
+++ packages/o-select-6/custom.vue
@@ -0,0 +1,103 @@
+<template lang="pug">
+	.widget-part(:style="styles" v-if="data")
+		.left.active(@click="back")
+		.main {{ time }}
+		.right.active(@click="next")
+</template>
+<script lang="ts">
+import { widgetMixin } from 'eslinkv-sdk'
+import { customConfig, value } from './index.component'
+import { Component, Watch } from 'vue-property-decorator'
+import { mixins } from 'vue-class-component'
+@Component
+export default class OSelect5 extends mixins(widgetMixin) {
+	time:number|string = 0
+
+	@Watch('time', { deep: true, immediate: true })
+	onDataChange(val) {
+		if (val) {
+			this.__handleClick__(val)
+		}
+	}
+
+	back() {
+		let year = new Date(this.time).getFullYear()
+		let month:any = new Date(this.time).getMonth()
+		if (month === 0) {
+			month = 12
+			year--
+		}
+		month = month > 9 ? month : `0${month}`
+		this.time = `${year}.${month}`
+	}
+
+	next() {
+		let year = new Date(this.time).getFullYear()
+		let month:any = new Date(this.time).getMonth() + 2
+		if (month === 13) {
+			month = 1
+			year++
+		}
+		month = month > 9 ? month : `0${month}`
+		this.time = `${year}.${month}`
+	}
+	
+	formatTime (date) {
+		const year = date.getFullYear()
+		let month:any = date.getMonth() + 1
+		month = month > 9 ? month : `0${month}`
+		return `${year}.${month}`
+	}
+
+	created() {
+		this.time = this.formatTime(new Date())
+		this.configValue = this.parseConfigValue(value, customConfig)
+	}
+}
+</script>
+<style lang="scss" scoped>
+.widget-part {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	
+	.main {
+		width: 86px;
+		height: 32px;
+		text-align: center;
+		line-height: 32px;
+		background: #0057A9;
+		border-radius: 4px;
+		font-size: 18px;
+		color: #FFFFFF;
+	}
+
+	.left {
+		width: 0;
+		height: 0;
+		border-top: 8px solid transparent;
+		border-right: 8px solid rgba(255, 255, 255, 0.4);
+		border-bottom: 8px solid transparent;
+		margin-right: 12px;
+		cursor: pointer;
+		
+		&.active {
+			border-right-color: #00DDFF;
+		}
+	}
+
+	.right {
+		width: 0;
+		height: 0;
+		border-top: 8px solid transparent;
+		border-bottom: 8px solid transparent;
+		border-left: 8px solid rgba(255, 255, 255, 0.4);
+		margin-left: 12px;
+		cursor: pointer;
+
+		&.active {
+			border-left-color: #00DDFF;
+		}
+	}
+}
+</style>
---
+++ packages/o-select-6/index.component.ts
@@ -0,0 +1,29 @@
+// 组件中文名唯一
+export const name = '选项卡6'
+// 组件名唯一
+export const type = 'o-select-6'
+// 检测当前组件名，版本唯一，类似npm发布策略
+export const version = '1.0.0'
+export const value = {
+	layout: {
+		size: {
+			width: 134,
+			height: 32,
+		},
+		position: {
+			value: 'relative',
+		},
+	},
+	api: {
+		data: JSON.stringify({})
+	},
+	config: {
+	},
+	widget: {
+		componentVersion: version,
+		name,
+	},
+}
+
+export const customConfig = [
+]
