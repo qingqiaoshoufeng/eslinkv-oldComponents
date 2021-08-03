9ee8edd8df04638ef39399bb8f3e29cd9d4d3fef
zhepei.shen <zhepei.shen@eslink.com>
2021-08-03 11:05:36 +0800

    feat: add 云版正式

--- esp-config.js
+++ esp-config.js
@@ -1,6 +1,8 @@
 module.exports = {
-	domain: 'http://eslinkv.eslink.cc/node/', // 云版
-	appKey: 'cb8bd7b1ffba4eeaa831a1df9be20715', // 云版
+	domain: 'http://eslinkv.eslink.cc/node/', // 云版 测试
+	appKey: 'cb8bd7b1ffba4eeaa831a1df9be20715', // 云版 测试
+	// domain: 'https://kv-etbc.eslink.com/node/', // 云版 正式
+	// appKey: '5f014d52d4ac4146b086dbf4a9452f88', // 云版 正式
 	// domain: 'http://192.168.1.44:2000/node/', // 杭燃 2000
 	// appKey: 'd31fc4dae41c4bb99be23e3513f92cbc', // 杭燃 2000
 	// domain: 'http://192.168.1.44:3000/node/', // 杭燃 3000
