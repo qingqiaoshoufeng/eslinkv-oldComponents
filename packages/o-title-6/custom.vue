574511af34e49da9ecb2da0c485884fc704be713
xjm <jiaming.xu@eslink.com>
2021-08-11 09:51:50 +0800

    feat: add

---
+++ packages/o-title-6/custom.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+widget-normal(:value="value", :customConfig="customConfig" class="main")
+	h2 {{ config.config.title ? config.config.title : data.title }}
+</template>
+<script lang="ts">
+import { widgetNormalMixin, widgetNormal } from '@eslinkv/vue2'
+import { Component } from 'vue-property-decorator'
+import { mixins } from 'vue-class-component'
+import { value, customConfig } from './index.component'
+
+@Component({ components: { widgetNormal } })
+export default class extends mixins(widgetNormalMixin) {
+value = value
+customConfig = customConfig
+}
+</script>
+<style lang="scss" scoped>
+h2 {
+	width: 100%;
+	height: 100%;
+	background: linear-gradient(90deg, rgba(0, 87, 169, 0.4) 0%, rgba(0, 87, 169, 0) 100%);
+	border-left: 4px solid #00DDFF;
+	padding-left: 8px;
+	text-align: left;
+	font-style: normal;
+	font-weight: 600;
+	font-size: 20px;
+	line-height: 24px;
+	color: #FFFFFF;
+	display: flex;
+	align-items: center;
+}
+</style>
---
+++ packages/o-title-6/index.component.ts
@@ -0,0 +1,38 @@
+// 组件中文名唯一
+export const name = '标题六'
+// 组件名唯一
+export const type = 'o-title-6'
+// 检测当前组件名，版本唯一，类似npm发布策略
+export const version = '1.0.0'
+// 自定义配置
+export const customConfig = [
+	{
+		prop: 'title',
+		label: '标题',
+		type: 'func-input',
+	},
+]
+// 配置
+export const value = {
+	api: {
+		data: JSON.stringify({ title: '标题文本' }),
+	},
+	layout: {
+		// 宽高
+		size: {
+			width: 480,
+			height: 40,
+		},
+		position: {
+			value: 'relative',
+		},
+	},
+	// 自定义配置默认值
+	config: {
+		title: '',
+	},
+	widget: {
+		componentVersion: version,
+		name,
+	},
+}
