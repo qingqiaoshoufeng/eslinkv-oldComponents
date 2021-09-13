b03037466d748c6a14c8b1a1e08c89e46a49b7b6
hy <hyuestu@163.com>
2021-09-13 17:56:17 +0800

    feat: 按钮切换

---
+++ packages/b-btn-1/custom.vue
@@ -0,0 +1,55 @@
+<template>
+	<widget-normal
+		:value="value"
+		:customConfig="customConfig"
+		:eventTypes="eventTypes"
+	>
+		<div class="b-btn-1" @click="handleClick">
+			<img src="./Vector.png" alt="" />
+		</div>
+	</widget-normal>
+</template>
+<script lang="ts">
+import { value, customConfig } from './index.component'
+import { widgetNormalMixin, widgetNormal } from '@eslinkv/vue2'
+
+export default {
+	name: 'b-btn-1',
+	mixins: [widgetNormalMixin],
+	components: { widgetNormal },
+	data() {
+		return {
+			eventTypes: [
+				{ key: 'click', label: '点击事件-隐藏' },
+				{ key: 'click1', label: '点击事件-显示' },
+			],
+			value,
+			customConfig,
+			showFlag: true,
+		}
+	},
+	methods: {
+		handleClick() {
+			if (this.showFlag) {
+				// 隐藏
+				this.__handleEvent__('click')
+				this.showFlag = false
+			} else {
+				this.__handleEvent__('click1')
+				// 显示
+				this.showFlag = true
+			}
+		},
+	},
+}
+</script>
+<style lang="scss" scoped>
+.b-btn-1 {
+	width: 100%;
+	height: 100%;
+	img {
+		width: 100%;
+		height: 100%;
+	}
+}
+</style>
---
+++ packages/b-btn-1/index.component.ts
@@ -0,0 +1,22 @@
+// 组件中文名唯一
+export const name = 'b-切换两侧'
+// 组件名唯一
+export const type = 'b-btn-1'
+// 检测当前组件名，版本唯一，类似npm发布策略
+export const version = '1.0.0'
+// 自定义配置
+export const customConfig = []
+// 配置
+export const value = {
+	widget: {
+		componentVersion: version,
+		name,
+	},
+	layout: {
+		// 宽高
+		size: {
+			width: 58,
+			height: 58,
+		},
+	},
+}
