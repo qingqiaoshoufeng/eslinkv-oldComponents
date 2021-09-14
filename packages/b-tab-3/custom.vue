42f714fad942a4bbe41b6f02769f2b852eeee074
hy <hyuestu@163.com>
2021-09-14 10:14:23 +0800

    feat: tab切换

---
+++ packages/b-tab-3/custom.vue
@@ -0,0 +1,55 @@
+<template>
+	<widget-normal :value="value" class="b-tab-3 fn-flex">
+		<div
+			v-for="item in data"
+			:key="item.id"
+			:class="{ active: item.id === editor.currentSceneIndex }"
+			@click="change(item.id)"
+		>
+			<span>{{ item.label }}</span>
+		</div>
+	</widget-normal>
+</template>
+<script lang="ts">
+import { widgetNormalMixin, widgetNormal } from '@eslinkv/vue2'
+import { Editor } from '@eslinkv/core'
+import { Component } from 'vue-property-decorator'
+import { mixins } from 'vue-class-component'
+import { value } from './index.component'
+
+@Component({ components: { widgetNormal } })
+export default class extends mixins(widgetNormalMixin) {
+	value = value
+	editor = Editor.Instance()
+
+	change(id) {
+		this.editor.selectSceneIndex(id)
+	}
+}
+</script>
+<style lang="scss" scoped>
+.b-tab-3 {
+	height: 100%;
+	div {
+		width: 122px;
+		height: 74px;
+		line-height: 74px;
+		word-break: break-all;
+		white-space: nowrap;
+		text-align: center;
+		border: 2px solid #74fff2;
+		box-sizing: border-box;
+		span {
+			font-size: 36px;
+			font-weight: 500;
+			color: #d2d2d2;
+		}
+		&.active {
+			background: rgba(116, 255, 242, 0.3);
+			span {
+				color: #fff;
+			}
+		}
+	}
+}
+</style>
---
+++ packages/b-tab-3/index.component.ts
@@ -0,0 +1,36 @@
+// 组件中文名唯一
+export const name = '切换三'
+// 组件名唯一
+export const type = 'b-tab-3'
+// 检测当前组件名，版本唯一，类似npm发布策略
+export const version = '1.0.1'
+// 自定义配置
+export const customConfig = []
+// 配置
+export const value = {
+	api: {
+		// 接口请求数据默认值
+		data: JSON.stringify([
+			{
+				label: '场站',
+				id: 1,
+			},
+			{
+				label: '管网',
+				id: 2,
+			},
+		]),
+	},
+	layout: {
+		// 宽高
+		size: {
+			width: 244,
+			height: 74,
+		},
+	},
+	config: {},
+	widget: {
+		componentVersion: version,
+		name,
+	},
+}
